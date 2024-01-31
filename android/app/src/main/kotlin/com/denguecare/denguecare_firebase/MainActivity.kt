dc4f844ae7423c08b9253d713596e3046e6d8dd6
Radoooooo <radooo1235@gmail.com>
2024-01-31 18:28:24 +0800

    commit

--- android/app/src/main/kotlin/com/denguecare/denguecare_firebase/MainActivity.kt
+++ android/app/src/main/kotlin/com/denguecare/denguecare_firebase/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.denguecare_firebase
+package com.denguecare.denguecare_firebase
 
 import io.flutter.embedding.android.FlutterActivity
 //import io.flutter.embedding.android.FlutterFragmentActivity
